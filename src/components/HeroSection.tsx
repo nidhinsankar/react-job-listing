2f595a304b4703bdf888595b534877ea49c35a0c
nidhinsankar <nidhinsankar1@gmail.com>
2024-10-15 19:38:43 +0530

    refactored hero section

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,6 +1,10 @@
 const HeroSection = () => {
   return (
-    <div className="w-full h-[140px] bg-gradient-to-br from-gray-200 to-red-300"></div>
+    <div className="w-full h-[140px] bg-gradient-to-br from-gray-200 to-red-300 flex justify-center items-center">
+      <h2 className="text-2xl md:text-5xl lg:text-7xl  font-bold uppercase">
+        React Job Listing
+      </h2>
+    </div>
   );
 };
 
